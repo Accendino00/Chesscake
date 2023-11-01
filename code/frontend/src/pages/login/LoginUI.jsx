33af6039a056f824372e6016a8b089afb2cd12cf
Tucano <donatidavide5@gmail.com>
2023-11-01 13:03:31 +0100

    Prima implementazione della pagina di Login

--- code/frontend/src/pages/login/LoginUI.jsx
+++ code/frontend/src/pages/login/LoginUI.jsx
@@ -1,25 +1,53 @@
 import React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import { TextField, Container, AppBar, Toolbar, Typography, Grid, Box } from '@mui/material';
+import Login from '@mui/icons-material/Login';
+
+import useStyles from './loginStyles';
 
 export class LoginUI extends React.Component {
     render (props) {
-        return (
-            <Box
-            component="form"
-            sx={{
-                '& .MuiTextField-root': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="off"
-            >
-                <TextField id="outlined-required" label="Username" variant="outlined" />
-                <TextField id="outlined-basic" label="Email" variant="outlined" type="password" />
-                <TextField id="outlined-basic" label="Password" variant="outlined" />
-                <TextField id="outlined-basic" label="Prova" variant="outlined" />
-            </Box>
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Login />
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Login
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <div>
+                    <Container
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                    >
+                        <Grid>
+                            <Grid item>
+                                <TextField id="outlined-required" label="Username" variant="outlined" />
+                            </Grid>
+                            <Grid item>
+                                <TextField id="outlined-basic" label="Email" variant="outlined" />
+                            </Grid>
+                            <Grid item>
+                                <TextField id="outlined-basic" label="Password" variant="outlined"  type="password" />
+                            </Grid>
+                            <Grid item>
+                                <TextField id="outlined-basic" label="Prova" variant="outlined" />
+                            </Grid>
+                        </Grid>
+                    </Container>
+                    <Button variant="contained">Login</Button>
+                </div>
+            </main>
+        </Box>
         );
     }
 }
 
 export default LoginUI;
+
