1e2cff3ae71b2d96d43afa2f752609b8d59e9ff3
Rinma Trupe <petrumarcel.marincas@studio.unibo.it>
2023-12-27 00:45:41 +0100

    Nuovo coverage e config

--- test/jest.config.js
+++ test/jest.config.js
@@ -43,10 +43,10 @@ const config = {
   coverageProvider: "v8",
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: [
-    "json",
-    "text",
     "lcov",
-    "clover"
+    // "json",
+    // "text",
+    // "clover"
   ],
 
   // An object that configures minimum threshold enforcement for coverage results
